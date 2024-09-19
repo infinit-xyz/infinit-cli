6aba540413de876586761322fab34c922bd68f02
Pitchanai Thitipakorn <pit_maplestory@hotmail.com>
2024-09-19 18:01:29 +0700

    Add compatibility for Windows OS

--- src/utils/childprocess.ts
+++ src/utils/childprocess.ts
@@ -1,8 +1,11 @@
 import { spawn } from 'node:child_process'
+import os from 'node:os'
 
 export const spawnChild = (cmd: string, args: string[], env?: NodeJS.ProcessEnv) => {
+  const isWindowsOs = os.platform() === 'win32'
+
   return new Promise((resolve, reject) => {
-    const child = spawn(cmd, args, { stdio: 'pipe', env: { ...process.env, ...env } })
+    const child = spawn(cmd, args, { stdio: 'pipe', env: { ...process.env, ...env }, ...(isWindowsOs ? { shell: true } : {}) })
     child.on('close', (code) => {
       if (code !== 0) {
         reject({
