a36b7b03e1cc7ea29122435c7f7a742dbbf931f1
melvin 🦊 <42515663+melvinmcrn@users.noreply.github.com>
2024-09-23 16:18:44 +0700

    chore(ci/cd): setup codecov (#18)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,16 +3,17 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 import type { CoverageReporter } from 'vitest/node'
 
-const coverageReporter: CoverageReporter[] = ['text', 'json-summary']
+const coverageReporter: CoverageReporter[] = ['text', 'json']
 
 export default defineConfig({
   test: {
     coverage: {
       reporter: coverageReporter,
-      provider: 'istanbul',
+      provider: 'v8',
       include: ['src/**/*'],
       exclude: ['**/*.test.*'],
     },
+    testTimeout: 25_000,
     env: loadEnv('', process.cwd(), ''),
   },
   plugins: [tsconfigPaths()],
