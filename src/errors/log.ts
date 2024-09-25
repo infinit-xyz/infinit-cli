2202555da8543743c681aa931dbc125c0da9e919
mintcnn <mintchnrt@gmail.com>
2024-09-25 13:35:02 +0700

    remove unused

--- src/errors/log.ts
+++ src/errors/log.ts
@@ -2,6 +2,4 @@ import { chalkError } from '@constants/chalk'
 
 export const customErrorLog = (error: Error) => {
   console.error(chalkError(error.message))
-  // console.log('')
-  // console.error(chalkError(error.stack))
 }
