c000a8985332401b066850b48e7948730644305a
melvinmcrn <greatwinblowaway@gmail.com>
2024-09-25 14:30:48 +0700

    update isCwdRootProject

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -93,13 +93,18 @@ export const checkFilesExist = (
 }
 
 /**
- * Check if the current working directory is the root of the project
- * @returns {boolean} true if the files are equal, false otherwise
+ * Determines if the current working directory (CWD) is the root of the project.
+ *
+ * This function checks if the CLI is running from the root directory of the project
+ * by comparing the CLI path with the current working directory and verifying the existence
+ * of specific configuration files.
+ *
+ * @param {string} [currentCwd=process.cwd()] - The current working directory. Defaults to the process's current working directory.
+ * @returns {Object} An object containing:
+ * - `currentCwd` (string): The current working directory.
+ * - `isRunningFromRootProject` (boolean): A flag indicating whether the CLI is running from the root project directory.
  */
-export const isCwdRootProject = () => {
-  // */project-name
-  const currentCwd = process.cwd()
-
+export const isCwdRootProject = (currentCwd = process.cwd()) => {
   // Get the path of the CLI which depending on the package manager
   // npm, bun, yarn: */<project-name>/node_modules/.bin/infinit
   // pnpm: */<project-name>/node_modules/<cli-package-name>/bin/cli.js
