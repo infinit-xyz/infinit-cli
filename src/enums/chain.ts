3cc4b26fee7d5719ba2165290266e94a0beb9ffa
melvinmcrn <greatwinblowaway@gmail.com>
2024-09-27 13:49:59 +0700

    add `BNB_Chain`

--- src/enums/chain.ts
+++ src/enums/chain.ts
@@ -3,7 +3,7 @@ export enum CHAIN_ID {
   Mantle = '5000',
   Sepolia = '11155111',
   Holesky = '17000',
-  // BSC = '56',
+  BNB_Chain = '56',
   // Arbitrum = '42161',
   // ARB_SEPOLIA = '421614',
   // Optimism = '10',
