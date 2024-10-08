a22dc5f85254c89e7a295117ad35eb33a9a167c1
Pitchanai Thitipakorn <pit_maplestory@hotmail.com>
2024-10-08 19:26:20 +0700

    add handler test

---
+++ src/commands/contract/verify/index.test.ts
@@ -0,0 +1,48 @@
+import { BufferedStream } from '@classes/BufferedStream/BufferedStream'
+import { completelyRemoveAnsi } from '@utils/ansi'
+import ora from 'ora'
+import { describe, expect, test, vi } from 'vitest'
+import { verifyContractCallbackHandler } from '.'
+
+const rm = completelyRemoveAnsi
+
+describe('contract verify', () => {
+  test('callback handler', () => {
+    // const spinner = {
+    //   start: vi.fn((v) => rm(v)).mockImplementation((v) => rm(v)),
+    //   stopAndPersist: vi.fn((v) => rm(v)),
+    //   fail: vi.fn(),
+    //   info: vi.fn(),
+    //   text: '',
+    // } as unknown as Ora
+    const customStream = new BufferedStream()
+
+    const spinner = ora({ spinner: 'dots', stream: customStream })
+    const callback = verifyContractCallbackHandler(spinner)
+    const consoleLogSpy = vi.spyOn(console, 'log').mockImplementation(() => undefined)
+
+    callback('contractVerificationInfo', { totalContracts: 3 })
+    expect(consoleLogSpy).toHaveBeenCalledWith('📝 Total contracts: 3')
+
+    callback('contractVerificationStarted', { contractName: 'contractName', address: '0x001' })
+    expect(rm(customStream.getLastChunk({ raw: true }))).toBe(`- (1/3) 🔍 Verifying... contractName (0x001)\n`)
+
+    callback('contractVerificationSubmitted', { contractName: 'contractName', address: '0x001' })
+    expect(rm(customStream.getLastChunk({ raw: true }))).toBe(`- (1/3) 🚀 Submitted contractName (0x001)\n`)
+
+    callback('contractVerificationFinished', { contractName: 'contractName', address: '0x001' })
+    expect(rm(customStream.getLastChunk({ raw: true }))).toBe(`✔ (1/3) Verified contractName (0x001)\n`)
+
+    callback('contractVerificationStarted', { contractName: 'contractName2', address: '0x002' })
+    expect(rm(customStream.getLastChunk({ raw: true }))).toBe(`- (2/3) 🔍 Verifying... contractName2 (0x002)\n`)
+
+    callback('contractVerificationFinished', { contractName: 'contractName2', address: '0x002' })
+    expect(rm(customStream.getLastChunk({ raw: true }))).toBe(`✔ (2/3) Verified contractName2 (0x002)\n`)
+
+    callback('contractVerificationStarted', { contractName: 'contractName3', address: '0x003' })
+    expect(rm(customStream.getLastChunk({ raw: true }))).toBe(`- (3/3) 🔍 Verifying... contractName3 (0x003)\n`)
+
+    callback('contractVerificationFinished', { contractName: 'contractName3', address: '0x003' })
+    expect(rm(customStream.getLastChunk({ raw: true }))).toBe(`🎉 All 3 contracts have been verified!\n`)
+  })
+})
--- src/commands/contract/verify/index.ts
+++ src/commands/contract/verify/index.ts
@@ -6,12 +6,52 @@ import type { ContractVerifierCallbackParams } from '@infinit-xyz/core/internal'
 import { getProjectChainInfo, getProjectRpc } from '@utils/config'
 import { readProjectRegistry } from '@utils/files'
 import chalk from 'chalk'
-import ora from 'ora'
+import ora, { type Ora } from 'ora'
 import { match } from 'ts-pattern'
 import { createPublicClient, http } from 'viem'
 import { explorerApiKeyPrompt, explorerApiUrlPrompt, explorerUrlPrompt } from './index.prompt'
 
 type CallbackParams = ContractVerifierCallbackParams
+
+export const verifyContractCallbackHandler = (spinner: Ora) => {
+  let totalContracts = 0
+  let verifiedContracts = 0
+
+  return async (key: keyof CallbackParams, value: CallbackParams[keyof CallbackParams]) => {
+    match(key)
+      .with('contractVerificationInfo', () => {
+        const parsedValue = value as CallbackParams['contractVerificationInfo']
+        totalContracts = parsedValue.totalContracts
+
+        console.log('')
+        console.log(`📝 Total contracts: ${totalContracts}`)
+      })
+      .with('contractVerificationStarted', () => {
+        const parsedValue = value as CallbackParams['contractVerificationStarted']
+        spinner.start(
+          `(${verifiedContracts + 1}/${totalContracts}) 🔍 Verifying... ${chalk.bold(chalkInfo(parsedValue.contractName))} ${chalk.dim(`(${parsedValue.address})`)}`,
+        )
+      })
+      .with('contractVerificationSubmitted', () => {
+        const parsedValue = value as CallbackParams['contractVerificationSubmitted']
+        spinner.start(
+          `(${verifiedContracts + 1}/${totalContracts}) 🚀 Submitted ${chalk.bold(chalkInfo(parsedValue.contractName))} ${chalk.dim(`(${parsedValue.address})`)}`,
+        )
+      })
+      .with('contractVerificationFinished', () => {
+        const parsedValue = value as CallbackParams['contractVerificationFinished']
+        spinner.succeed(
+          `(${verifiedContracts + 1}/${totalContracts}) Verified ${chalk.bold(chalkInfo(parsedValue.contractName))} ${chalk.dim(`(${parsedValue.address})`)}`,
+        )
+        verifiedContracts++
+
+        if (verifiedContracts === totalContracts) {
+          spinner.stopAndPersist({ symbol: '🎉', text: `All ${totalContracts} contract${totalContracts > 1 ? 's' : ''} have been verified!` })
+        }
+      })
+      .exhaustive()
+  }
+}
 
 export const handleVerifyContract = async () => {
   const projectConfig = config.getProjectConfig()
@@ -47,45 +87,9 @@ export const handleVerifyContract = async () => {
     url: explorerUrl,
   })
 
-  let totalContracts = 0
-  let verifiedContracts = 0
-
   // Spinner
   const spinner = ora({ spinner: 'dots' })
 
   // Verify
-  verifier.verify(registry, async (key: keyof CallbackParams, value: CallbackParams[keyof CallbackParams]) => {
-    match(key)
-      .with('contractVerificationInfo', () => {
-        const parsedValue = value as CallbackParams['contractVerificationInfo']
-        totalContracts = parsedValue.totalContracts
-
-        console.log('')
-        console.log(`📝 Total contracts: ${totalContracts}`)
-      })
-      .with('contractVerificationStarted', () => {
-        const parsedValue = value as CallbackParams['contractVerificationStarted']
-        spinner.start(
-          `(${verifiedContracts + 1}/${totalContracts}) 🔍 Verifying... ${chalk.bold(chalkInfo(parsedValue.contractName))} ${chalk.dim(`(${parsedValue.address})`)}`,
-        )
-      })
-      .with('contractVerificationSubmitted', () => {
-        const parsedValue = value as CallbackParams['contractVerificationSubmitted']
-        spinner.start(
-          `(${verifiedContracts + 1}/${totalContracts}) 🚀 Submitted ${chalk.bold(chalkInfo(parsedValue.contractName))} ${chalk.dim(`(${parsedValue.address})`)}`,
-        )
-      })
-      .with('contractVerificationFinished', () => {
-        const parsedValue = value as CallbackParams['contractVerificationFinished']
-        spinner.succeed(
-          `(${verifiedContracts + 1}/${totalContracts}) Verified ${chalk.bold(chalkInfo(parsedValue.contractName))} ${chalk.dim(`(${parsedValue.address})`)}`,
-        )
-        verifiedContracts++
-
-        if (verifiedContracts === totalContracts) {
-          spinner.stopAndPersist({ symbol: '🎉', text: `All ${totalContracts} contract${totalContracts > 1 ? 's' : ''} have been verified!` })
-        }
-      })
-      .exhaustive()
-  })
+  verifier.verify(registry, verifyContractCallbackHandler(spinner))
 }
