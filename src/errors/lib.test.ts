fc3764207ac8fa8557348fa0b5fdbc11cf1c798c
melvinmcrn <greatwinblowaway@gmail.com>
2024-10-09 14:58:45 +0700

    update INFINITLibraryError

--- src/errors/lib.test.ts
+++ src/errors/lib.test.ts
@@ -2,19 +2,19 @@ import { name as cliName, version as cliVersion } from 'package.json'
 import { describe, expect, test } from 'vitest'
 
 import { PROTOCOL_MODULE } from '@enums/module'
-import { ProtocolModuleLibError } from '@errors/index'
+import { INFINITLibraryError } from '@errors/index'
 
-describe('ProtocolModuleLibError', () => {
+describe('INFINITLibraryError', () => {
   const nodeVersion = process.versions.node
   test('should match snapshot', () => {
-    expect(new ProtocolModuleLibError(PROTOCOL_MODULE.aave_v3, 'error')).toMatchInlineSnapshot(`
-        [ProtocolModuleLibError-${PROTOCOL_MODULE.aave_v3}: error
+    expect(new INFINITLibraryError(PROTOCOL_MODULE.aave_v3, 'error')).toMatchInlineSnapshot(`
+        [INFINITLibraryError-${PROTOCOL_MODULE.aave_v3}: error
         ${cliName}: ${cliVersion}
         Node: ${nodeVersion}]
     `)
 
-    expect(new ProtocolModuleLibError(PROTOCOL_MODULE.token, 'error')).toMatchInlineSnapshot(`
-        [ProtocolModuleLibError-${PROTOCOL_MODULE.token}: error
+    expect(new INFINITLibraryError(PROTOCOL_MODULE.token, 'error')).toMatchInlineSnapshot(`
+        [INFINITLibraryError-${PROTOCOL_MODULE.token}: error
         ${cliName}: ${cliVersion}
         Node: ${nodeVersion}]
     `)
--- src/errors/lib.ts
+++ src/errors/lib.ts
@@ -1,10 +1,10 @@
 import type { PROTOCOL_MODULE } from '@enums/module'
 import { BaseError } from '@errors/base'
 
-export class ProtocolModuleLibError extends BaseError {
+export class INFINITLibraryError extends BaseError {
   constructor(protocolModule: PROTOCOL_MODULE, errorMsg: string = '') {
     super(errorMsg, {
-      name: `ProtocolModuleLibError-${protocolModule}`,
+      name: `INFINITLibraryError-${protocolModule}`,
     })
   }
 }
