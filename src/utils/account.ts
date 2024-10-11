51e3fff70148bb586553c4c6eae7c6d8d6473f1b
melvinmcrn <greatwinblowaway@gmail.com>
2024-10-11 14:30:55 +0700

    refactor `utils/account.ts`

--- src/utils/account.ts
+++ src/utils/account.ts
@@ -1,26 +1,47 @@
+import fs from 'fs'
+import path from 'path'
+
 import { DATA_FOLDER } from '@classes/Config/Config'
 import { createDataFolder } from '@utils/config'
 import { getFilesCurrentDir } from '@utils/files'
-import fs from 'fs'
-import path from 'path'
+
+const ACCOUNT_FILE_EXTENSION = '.json'
+
+export const isValidAccountFileName = (str?: string) => !!str && /^[\w\-. ]+\.json$/.test(str)
+
+export const getAccountIdFromAccountFileName = (fileName: string) => {
+  if (!isValidAccountFileName(fileName)) {
+    throw new Error(`Invalid account file name: ${fileName}`)
+  }
+
+  return fileName.slice(0, -ACCOUNT_FILE_EXTENSION.length)
+}
+
+export const getAccountFileNameFromAccountId = (accountId: string) => `${accountId}${ACCOUNT_FILE_EXTENSION}`
+
+export const getAccountsFolderPath = () => path.resolve(DATA_FOLDER, 'accounts')
 
 /**
  * Check if the account ID is found among existing accounts.
  * @param accountId
  * @returns
  */
-export const checkIsAccountFound = (accountId: string) => {
-  const accountsFolderPath = path.resolve(DATA_FOLDER, 'accounts')
+export const checkIsAccountFound = (accountId: string): boolean => {
+  const accountsFolderPath = getAccountsFolderPath()
+
   if (!fs.existsSync(accountsFolderPath)) {
     createDataFolder()
-    return
+    return false
   }
+
   const files = getFilesCurrentDir(accountsFolderPath)
-  return files.includes(`${accountId}.json`)
+  return files.includes(getAccountFileNameFromAccountId(accountId))
 }
 
 export const getAccountsList = () => {
-  const accountsFolderPath = path.join(DATA_FOLDER, 'accounts')
+  const accountsFolderPath = getAccountsFolderPath()
+
   const files = getFilesCurrentDir(accountsFolderPath)
+
   return { accountFiles: files, accountsFolderPath }
 }
