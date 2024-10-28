a3da2f2f4e0889d58d117f4b6e0cf549c933c5f7
mintcnn <mintchnrt@gmail.com>
2024-10-28 18:13:37 +0700

    update verifier type and update dependency

--- src/constants/protocol-module.type.ts
+++ src/constants/protocol-module.type.ts
@@ -1,8 +1,6 @@
 import type { PROTOCOL_MODULE } from '@enums/module'
-import type { AaveV3ContractVerifier } from '@infinit-xyz/aave-v3/utils'
 import type { InfinitActionRecord } from '@infinit-xyz/core'
-import type { TokenContractVerifier } from '@infinit-xyz/token/utils'
-import type { UniswapV3ContractVerifier } from '@infinit-xyz/uniswap-v3/utils'
+import type { BaseContractVerifier } from '@infinit-xyz/core/internal'
 import type { dependencies } from 'package.json'
 
 export type ModuleAction = {
@@ -11,5 +9,5 @@ export type ModuleAction = {
   description: string
   actions: InfinitActionRecord
   libPath: keyof typeof dependencies
-  Verifier: typeof TokenContractVerifier | typeof AaveV3ContractVerifier | typeof UniswapV3ContractVerifier
+  Verifier: typeof BaseContractVerifier
 }
