93a86418f7c751fc20b47156fc0d0be658072055
mewpawee <pawee@alphainnovationslab.io>
2025-01-09 15:00:28 +0700

    add get contract root test

--- src/utils/files/getContractRoot.test.ts
+++ src/utils/files/getContractRoot.test.ts
@@ -1,1 +1,11 @@
-//TODO: write the test
+import { describe, expect, test } from 'vitest'
+import { PROTOCOL_MODULE } from '@enums/module'
+
+describe('getContractRoot', () => {
+  test('should ', () => {
+    for (const protocolModule in PROTOCOL_MODULE) {
+      const module = protocolModule.replaceAll('_', '-')
+      expect(module.includes('_')).toBeFalsy()
+    }
+  })
+})
