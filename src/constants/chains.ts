f75b09993a22d36ca55ac5660f29862584656a7d
melvinmcrn <greatwinblowaway@gmail.com>
2025-01-24 14:43:24 +0700

    add support for monad testnet

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,5 +1,5 @@
 import { CHAIN_ID } from '@enums/chain'
-import type { Chain } from 'viem'
+import { type Chain, defineChain } from 'viem'
 import * as viemChains from 'viem/chains'
 
 type TokenDetail = {
@@ -19,6 +19,31 @@ export type ChainInfo = {
     instance: Chain
   }
 }
+
+const monadTestnetChain = defineChain({
+  id: parseInt(CHAIN_ID.Monad_Testnet),
+  name: 'Monad Testnet',
+  nativeCurrency: {
+    name: 'Monad',
+    symbol: 'MON',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-testnet.monadinfra.com/rpc/smkuKxR14Php4gxcWPU7ZZk5DEd9xXBU'],
+      webSocket: [],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://explorer.monad-testnet.category.xyz' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 251449,
+    },
+  },
+})
 
 export const CHAINS: Record<CHAIN_ID, ChainInfo> = {
   [CHAIN_ID.Ethereum]: {
@@ -124,7 +149,7 @@ export const CHAINS: Record<CHAIN_ID, ChainInfo> = {
     chainId: CHAIN_ID.Unichain_Sepolia,
     name: '[Testnet] Unichain Sepolia',
     shortName: 'Unichain Sepolia',
-    rpcList: ['	https://sepolia.unichain.org'],
+    rpcList: ['https://sepolia.unichain.org'],
     nativeCurrency: {
       name: 'Ethereum',
       symbol: 'ETH',
@@ -132,6 +157,20 @@ export const CHAINS: Record<CHAIN_ID, ChainInfo> = {
     },
     viemChain: {
       instance: viemChains.unichainSepolia,
+    },
+  },
+  [CHAIN_ID.Monad_Testnet]: {
+    chainId: CHAIN_ID.Monad_Testnet,
+    name: '[Testnet] Monad Testnet',
+    shortName: 'Monad Testnet',
+    rpcList: ['https://rpc-testnet.monadinfra.com/rpc/smkuKxR14Php4gxcWPU7ZZk5DEd9xXBU'],
+    nativeCurrency: {
+      name: 'Monad',
+      symbol: 'MON',
+      decimals: 18,
+    },
+    viemChain: {
+      instance: monadTestnetChain,
     },
   },
   // [CHAIN_ID.Arbitrum]: {
